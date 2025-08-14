347883c3f45dfa625fda5a275327b0c20133fb84
berthus <bernardosan13@gmail.com>
2025-08-14 14:54:15 -0500

    v0.0_inicial_berthus_14082025

---
+++ src/main/java/com/sena/ecommerce/model/Usuario.java
@@ -0,0 +1,136 @@
+package com.sena.ecommerce.model;
+
+import java.util.List;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedEntityGraph;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@NamedEntityGraph
+@Table (name = "usuario")
+
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Integer id;
+	private String nombre;
+	private String username;
+	private String imail;
+	private String documento;
+	private String direccion;
+	private String telefono;
+	private String password;
+	private String tipo;
+	
+	@OneToMany(mappedBy = "usuarios")
+	private List<Producto> productos;
+	
+	@OneToMany(mappedBy = "usuarios")
+	private List<Orden> ordenes;
+	
+	public Usuario() {
+		
+	}
+
+	public Usuario(Integer id, String nombre, String username, String imail, String documento, String direccion,
+			String telefono, String password, String tipo) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.username = username;
+		this.imail = imail;
+		this.documento = documento;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.password = password;
+		this.tipo = tipo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getImail() {
+		return imail;
+	}
+
+	public void setImail(String imail) {
+		this.imail = imail;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", username=" + username + ", imail=" + imail
+				+ ", documento=" + documento + ", direccion=" + direccion + ", telefono=" + telefono + ", password="
+				+ password + ", tipo=" + tipo + "]";
+	}
+
+
+	
+
+}
