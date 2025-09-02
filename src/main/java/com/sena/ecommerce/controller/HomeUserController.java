ed21054662ec8870f75b9f361d59dfff3657a7f2
berthus <bernardosan13@gmail.com>
2025-09-02 14:15:43 -0500

    v0.5_vistaProductoHome_classHomeUerControllerMetodoProductoHome_berthu_02092025

--- src/main/java/com/sena/ecommerce/controller/HomeUserController.java
+++ src/main/java/com/sena/ecommerce/controller/HomeUserController.java
@@ -1,16 +1,25 @@
 package com.sena.ecommerce.controller;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.sena.ecommerce.model.Producto;
 import com.sena.ecommerce.service.IProductoService;
 
 @Controller
 @RequestMapping("/")
 public class HomeUserController {
+
+	// instancia de logger
+
+	private final Logger LOGGER = (Logger) LoggerFactory.getLogger(HomeUserController.class);
 
 	// instancia de productoService
 	@Autowired
@@ -20,6 +29,21 @@ public class HomeUserController {
 	public String home(Model model) {
 		model.addAttribute("productos", productoService.findAll());
 		return "usuarios/home";
+	}
+
+	// metodo que carga el producto de usuario con id producto
+	@GetMapping("productohome/{id}")
+	public String productohome(@PathVariable Integer id, Model model) {
+		LOGGER.info("id producto enviado como parametro {}", id);
+
+		// variable de producto
+		Producto p = new Producto();
+		Optional<Producto> op = productoService.get(id);
+
+		// pasar el producto
+		p = op.get();
+		model.addAttribute("productos", p);
+		return "usuarios/productohome";
 	}
 
 }
